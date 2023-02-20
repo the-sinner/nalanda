c318033bf622c7e323c8eb41211b5e530c8cb593
Shalabh Agarwal <shalabhagarwal1024@gmail.com>
2023-02-20 20:30:38 +0530

    Initial Commit

---
+++ book-data.js
@@ -0,0 +1,16 @@
+const data = {
+    "books": [
+        {
+            "id":"1",
+            "name":"The Three Body Problem",
+            "cover":"3bp.jpeg",
+            "epub":"three-body-problem"
+        },
+        {
+            "id":"2",
+            "name":"Frankstein",
+            "cover":"3bp.jpeg",
+            "epub":"three-body-problem"
+        }
+    ]
+}
